8d7b8a420f61835fc9121dd33d05fc0fb5dfe66e
samgoodrick <samgoodrick@lock3software.com>
2018-07-23 13:39:37 -0400

    add static assertions on reflection traits

--- test/CXX/meta/reflexpr_common.cpp
+++ test/CXX/meta/reflexpr_common.cpp
@@ -44,12 +44,24 @@ int main(int argc, char* argv[]) {
   {
     constexpr meta::info r = reflexpr(x1);
     static_assert(kind(r) == meta::variable_decl);
+    static_assert(has_external_linkage(r) == true);
+    static_assert(has_static_storage(r) == true);
+    static_assert(is_extern(r) == false);
+    static_assert(is_static(r) == false);
+    static_assert(is_inline(r) == false);
+    static_assert(is_constexpr(r) == false);
   }
 
   // static int x2 -- global
   {
     constexpr meta::info r = reflexpr(x2);
     static_assert(kind(r) == meta::variable_decl);
+    static_assert(has_internal_linkage(r) == true);
+    static_assert(has_static_storage(r) == true);
+    static_assert(is_extern(r) == false);
+    static_assert(is_static(r) == true);
+    static_assert(is_inline(r) == false);
+    static_assert(is_constexpr(r) == false);
   }
 
   // Functions
@@ -58,30 +70,63 @@ int main(int argc, char* argv[]) {
   {
     constexpr meta::info r = reflexpr(f1);
     static_assert(kind(r) == meta::function_decl);
+    static_assert(has_external_linkage(r) == true);
+    static_assert(is_static(r) == false);
+    static_assert(is_extern(r) == false);
+    static_assert(is_constexpr(r) == false);
+    static_assert(is_defined(r) == true);
+    static_assert(is_inline(r) == false);
+    static_assert(is_deleted(r) == false);
   }
 
   // void f2();
   {
     constexpr meta::info r = reflexpr(f2);
     static_assert(kind(r) == meta::function_decl);
+    static_assert(has_external_linkage(r) == true);
+    static_assert(is_static(r) == false);
+    static_assert(is_extern(r) == false);
+    static_assert(is_constexpr(r) == false);
+    static_assert(is_defined(r) == false);
   }
 
   // static inline void f3() { }
   {
     constexpr meta::info r = reflexpr(f3);
     static_assert(kind(r) == meta::function_decl);
+    static_assert(has_internal_linkage(r) == true);
+    static_assert(is_static(r) == true);
+    static_assert(is_extern(r) == false);
+    static_assert(is_constexpr(r) == false);
+    static_assert(is_defined(r) == true);
+    static_assert(is_inline(r) == true);
+    static_assert(is_deleted(r) == false);
   }
 
   // constexpr int f4() { return 0; }
   {
     constexpr meta::info r = reflexpr(f4);
     static_assert(kind(r) == meta::function_decl);
+    static_assert(has_external_linkage(r) == true);
+    static_assert(is_static(r) == false);
+    static_assert(is_extern(r) == false);
+    static_assert(is_constexpr(r) == true);
+    static_assert(is_defined(r) == true);
+    static_assert(is_inline(r) == true);
+    static_assert(is_deleted(r) == false);
   }
 
   // void f5(int) = delete;
   {
     constexpr meta::info r = reflexpr(f5);
     static_assert(kind(r) == meta::function_decl);
+    static_assert(has_external_linkage(r) == true);
+    static_assert(is_static(r) == false);
+    static_assert(is_extern(r) == false);
+    static_assert(is_constexpr(r) == false);
+    static_assert(is_defined(r) == true);
+    static_assert(is_inline(r) == true); // FIXME: Apparently so.
+    static_assert(is_deleted(r) == true);
   }
 
   // Namespaces
@@ -90,12 +135,14 @@ int main(int argc, char* argv[]) {
   {
     constexpr meta::info r = reflexpr(N);
     static_assert(kind(r) == meta::namespace_decl);
+    static_assert(is_inline(r) == false);
   }
 
   // inline namespace N::M
   {
     constexpr meta::info r = reflexpr(N::M);
     static_assert(kind(r) == meta::namespace_decl);
+    static_assert(is_inline(r) == true);
   }
 
   // Classes
@@ -104,30 +151,43 @@ int main(int argc, char* argv[]) {
   {
     constexpr meta::info r = reflexpr(C1);
     static_assert(kind(r) == meta::class_decl);
+    static_assert(has_external_linkage(r) == true);
+    static_assert(has_access(r) == false);
+    static_assert(is_class(r) == true);
+    static_assert(is_declared_class(r) == true);
+    static_assert(is_complete(r) == true);
   }
 
   // class C2;
   {
     constexpr meta::info r = reflexpr(C2);
     static_assert(kind(r) == meta::class_decl);
+    static_assert(has_external_linkage(r) == true);
+    static_assert(has_access(r) == false);
+    static_assert(is_class(r) == true);
+    static_assert(is_complete(r) == false);
   }
 
   // struct S1 { ... };
   {
     constexpr meta::info r = reflexpr(S1);
     static_assert(kind(r) == meta::class_decl);
+    static_assert(is_declared_struct(r) == true);
   }
 
   // class S1::C { ... };
   {
     constexpr meta::info r = reflexpr(S1::C);
     static_assert(kind(r) == meta::class_decl);
+    static_assert(has_access(r) == true);
+    static_assert(is_public(r) == true);
   }
   
   // union U1 { ... };
   {
     constexpr meta::info r = reflexpr(U1);
     static_assert(kind(r) == meta::class_decl);
+    static_assert(is_union(r) == true);
   }
 
   // Data members
@@ -136,30 +196,45 @@ int main(int argc, char* argv[]) {
   {
     constexpr meta::info r = reflexpr(S1::x1);
     static_assert(kind(r) == meta::data_member_decl);
+    static_assert(is_public(r) == true);
+    static_assert(is_static(r) == false);
+    static_assert(is_mutable(r) == false);
+    static_assert(is_inline(r) == false);
+    static_assert(is_constexpr(r) == false);
+    static_assert(is_bitfield(r) == false);
   }
 
   // mutable int S1::x2;
   {
     constexpr meta::info r = reflexpr(S1::x2);
     static_assert(kind(r) == meta::data_member_decl);
+    static_assert(is_static(r) == false);
+    static_assert(is_mutable(r) == true);
   }
 
   // static int S1::x3;
   {
     constexpr meta::info r = reflexpr(S1::x3);
     static_assert(kind(r) == meta::data_member_decl);
+    static_assert(is_static(r) == true);
+    static_assert(is_mutable(r) == false);
   }
 
   // static constexpr int S1::x4;
   {
     constexpr meta::info r = reflexpr(S1::x4);
     static_assert(kind(r) == meta::data_member_decl);
+    static_assert(is_static(r) == true);
+    static_assert(is_inline(r) == true);
+    static_assert(is_constexpr(r) == true);
   }
 
   // int S1::x5 : 4;
   {
     constexpr meta::info r = reflexpr(S1::x5);
     static_assert(kind(r) == meta::data_member_decl);
+    static_assert(is_static(r) == false);
+    static_assert(is_bitfield(r) == true);
   }
 
   // Member functions
@@ -168,6 +243,17 @@ int main(int argc, char* argv[]) {
   {
     constexpr meta::info r = reflexpr(S1::f1);
     static_assert(kind(r) == meta::member_function_decl);
+    static_assert(is_public(r) == true);
+    static_assert(is_normal(r) == true);
+    static_assert(is_static(r) == false);
+    static_assert(is_constexpr(r) == false);
+    static_assert(is_virtual(r) == false);
+    static_assert(is_pure_virtual(r) == false);
+    static_assert(is_override(r) == false);
+    static_assert(is_final(r) == false);
+    static_assert(is_defined(r) == true);
+    static_assert(is_inline(r) == true); // in-class members are implicitly inline
+    static_assert(is_deleted(r) == false);
   }
 
   // FIXME: Add more tests for member functions.
@@ -178,18 +264,27 @@ int main(int argc, char* argv[]) {
   {
     constexpr meta::info r = reflexpr(E1);
     static_assert(kind(r) == meta::enum_decl);
+    static_assert(has_external_linkage(r) == true);
+    static_assert(has_access(r) == false);
+    static_assert(is_scoped(r) == false);
   }
 
   // enum class E2 { ... }
   {
     constexpr meta::info r = reflexpr(E2);
     static_assert(kind(r) == meta::enum_decl);
+    static_assert(has_external_linkage(r) == true);
+    static_assert(has_access(r) == false);
+    static_assert(is_scoped(r) == true);
   }
 
   // enum class : int;
   {
     constexpr meta::info r = reflexpr(E3);
     static_assert(kind(r) == meta::enum_decl);
+    static_assert(has_external_linkage(r) == true);
+    static_assert(has_access(r) == false);
+    static_assert(is_scoped(r) == true);
   }
 
   // Enumerators
@@ -198,12 +293,14 @@ int main(int argc, char* argv[]) {
   {
     constexpr meta::info r = reflexpr(X);
     static_assert(kind(r) == meta::enumerator_decl);
+    static_assert(has_access(r) == false);
   }
 
   // enum class E2 { X }
   {
     constexpr meta::info r = reflexpr(E2::X);
     static_assert(kind(r) == meta::enumerator_decl);
+    static_assert(has_access(r) == false);
   }
 
 }
