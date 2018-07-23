d803275924b304729db9865ffd2fd6248f63d0b3
samgoodrick <samgoodrick@lock3software.com>
2018-07-23 11:50:57 -0400

    testing some reflections for common entities and dependent common entities

---
+++ test/CXX/meta/reflexpr.cpp
@@ -0,0 +1,60 @@
+// RUN: %clang -std=c++1z %s
+
+#include <experimental/meta>
+
+using namespace std::experimental;
+
+namespace Ok {
+
+struct S { };
+enum E { e1, e2, e3 };
+
+namespace N {
+  namespace M { }
+}
+
+void f(int n) {
+  constexpr meta::info meta1 = reflexpr(n);
+  constexpr meta::info meta2 = reflexpr(f);
+
+  double dub = 42.0;
+  char character = 'a';
+  
+  constexpr meta::info meta3 = reflexpr(dub);
+  constexpr meta::info meta4 = reflexpr(character);
+}
+
+}
+
+namespace Bad {
+
+void g();
+void g(int);
+
+template<typename T> void g2(T);
+
+void f() {
+  // reflexpr(x); // expected-error {{reflection of undeclared identifier 'x'}}
+  // reflexpr(g); // expected-error {{reflection of overloaded identifier 'g'}}
+  // reflexpr(g2); // expected-error {{reflection of overloaded identifier 'g2'}}
+}
+
+}
+
+namespace OkDependent {
+
+struct product{};
+  
+template<typename T>
+constexpr int test() {
+  meta::info x = reflexpr(T);
+  return 0;
+}
+
+void test_templates() {
+  constexpr int x1 = test<int>();
+  constexpr int x2 = test<product>();
+
+}
+  
+}
