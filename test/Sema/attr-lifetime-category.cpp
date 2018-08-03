a70e97efdcef38657deb4c3f90d0cf1729de0254
Matthias Gehre <gehre.matthias@gmail.com>
2018-08-03 22:40:27 +0200

    clang-format

--- test/Sema/attr-lifetime-category.cpp
+++ test/Sema/attr-lifetime-category.cpp
@@ -3,83 +3,83 @@
 // TODO: regression tests should not include the standard libary,
 //       but we should have a way to test against real implementations.
 namespace std {
-  struct any {};
-
-  template<typename T>
-  struct basic_regex {};
-
-  template<typename T>
-  struct regex_iterator {
-    T &operator*() const;
-  };
-
-  using regex = basic_regex<char>;
-
-  template<typename T>
-  struct unique_ptr {
-    T &operator*() const;
-    ~unique_ptr();
-  };
-
-  template<typename T>
-  struct optional {};
-
-  template<typename T>
-  struct vector {
-    T *begin();
-    T *end();
-    ~vector();
-  };
-
-  struct _Bit_reference {};
-
-  template<>
-  struct vector<bool> {
-    using reference = _Bit_reference;
-    bool *begin();
-    bool *end();
-    ~vector();
-  };
-
-  template<typename T>
-  struct span {
-    T *begin();
-    T *end();
-  };
-
-  template<typename T>
-  struct basic_string_view {
-    T *begin();
-    T *end();
-  };
-
-  using string_view = basic_string_view<char>;
-
-  template<typename T>
-  struct stack {};
-
-  template<typename T>
-  struct queue {};
-
-  template<typename T>
-  struct priority_queue {};
-
-  template<typename... T>
-  struct variant {};
-
-  template<typename T>
-  struct reference_wrapper {
-    template<typename U>
-    reference_wrapper(U&&);
-    operator T &() const;
-  };
-}
+struct any {};
 
 template <typename T>
-void __lifetime_type_category(){}
+struct basic_regex {};
 
 template <typename T>
-void __lifetime_type_category_arg(T arg){}
+struct regex_iterator {
+  T &operator*() const;
+};
+
+using regex = basic_regex<char>;
+
+template <typename T>
+struct unique_ptr {
+  T &operator*() const;
+  ~unique_ptr();
+};
+
+template <typename T>
+struct optional {};
+
+template <typename T>
+struct vector {
+  T *begin();
+  T *end();
+  ~vector();
+};
+
+struct _Bit_reference {};
+
+template <>
+struct vector<bool> {
+  using reference = _Bit_reference;
+  bool *begin();
+  bool *end();
+  ~vector();
+};
+
+template <typename T>
+struct span {
+  T *begin();
+  T *end();
+};
+
+template <typename T>
+struct basic_string_view {
+  T *begin();
+  T *end();
+};
+
+using string_view = basic_string_view<char>;
+
+template <typename T>
+struct stack {};
+
+template <typename T>
+struct queue {};
+
+template <typename T>
+struct priority_queue {};
+
+template <typename... T>
+struct variant {};
+
+template <typename T>
+struct reference_wrapper {
+  template <typename U>
+  reference_wrapper(U &&);
+  operator T &() const;
+};
+} // namespace std
+
+template <typename T>
+void __lifetime_type_category() {}
+
+template <typename T>
+void __lifetime_type_category_arg(T arg) {}
 
 class [[gsl::Owner]] my_owner {
   int i;
@@ -90,9 +90,9 @@ class [[gsl::Pointer]] my_pointer {
 };
 
 struct my_implicit_owner {
-    int *begin() const;
-    int *end() const;
-    ~my_implicit_owner();
+  int *begin() const;
+  int *end() const;
+  ~my_implicit_owner();
 };
 
 struct my_derived_owner : my_implicit_owner {
@@ -110,9 +110,9 @@ void owner() {
   __lifetime_type_category<decltype(std::variant<int, char *>())>(); // expected-warning {{Owner}}
   __lifetime_type_category<decltype(std::any())>();                  // expected-warning {{Owner}}
   using IntVector = std::vector<int>;
-  __lifetime_type_category<decltype(IntVector())>();                 // expected-warning {{Owner}}
-  __lifetime_type_category<decltype(my_implicit_owner())>();         // expected-warning {{Owner}}
-  __lifetime_type_category<decltype(my_derived_owner())>();          // expected-warning {{Owner}}
+  __lifetime_type_category<decltype(IntVector())>();         // expected-warning {{Owner}}
+  __lifetime_type_category<decltype(my_implicit_owner())>(); // expected-warning {{Owner}}
+  __lifetime_type_category<decltype(my_derived_owner())>();  // expected-warning {{Owner}}
 }
 
 void pointer() {
