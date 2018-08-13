f79164f8ca46f0aae93eb2a5acc0cc295a842867
Wyatt Childers <wchilders@lock3software.com>
2018-08-13 16:19:31 -0400

    Add relevant codegen for C++ Fragment and Fragment Decl

--- include/clang/AST/RecursiveASTVisitor.h
+++ include/clang/AST/RecursiveASTVisitor.h
@@ -1957,6 +1957,10 @@ DEF_TRAVERSE_DECL(BindingDecl, {
 
 DEF_TRAVERSE_DECL(MSPropertyDecl, { TRY_TO(TraverseDeclaratorHelper(D)); })
 
+DEF_TRAVERSE_DECL(CXXFragmentDecl, {
+  TRY_TO(TraverseDecl(D->getContent()));
+});
+
 DEF_TRAVERSE_DECL(FieldDecl, {
   TRY_TO(TraverseDeclaratorHelper(D));
   if (D->isBitField())
