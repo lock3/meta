0010ed729c1b691a32aef4292b9d52b5ee1a6732
samgoodrick <samgoodrick@lock3software.com>
2018-08-16 12:58:17 -0400

    Testing of printing entity names as strings.

---
+++ test/CXX/meta/to_string.cpp
@@ -0,0 +1,48 @@
+// RUN: %clang -std=c++1z %s
+
+#include <experimental/meta>
+
+using namespace std::experimental;
+
+extern "C" int printf(const char*, ...);
+
+enum E {
+  X, A, B, C
+};
+
+
+template<meta::info X, typename T>
+const char* find_enumerator(T val)
+{
+  if constexpr (!is_null(X)) {
+    if (valueof(X) == val)
+      return name(X);
+    return find_enumerator<next(X)>(val);
+  }
+  return nullptr;
+}
+
+template<typename T>
+const char* enum_to_string(T val) {
+  return find_enumerator<meta::front(reflexpr(T))>(val);
+}
+
+int main() {
+  {  
+    const char* s = enum_to_string(A);
+    assert(s);
+    printf("%s\n", s);
+  }
+
+  {  
+    const char* s = enum_to_string(B);
+    assert(s);
+    printf("%s\n", s);
+  }
+
+  {  
+    const char* s = enum_to_string(C);
+    assert(s);
+    printf("%s\n", s);
+  }
+}
