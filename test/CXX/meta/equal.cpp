6985b8a5142613e4a48b7749afe9afd7cd5dbdce
samgoodrick <samgoodrick@lock3software.com>
2018-08-16 12:59:40 -0400

    Testing for equality of entities

---
+++ test/CXX/meta/equal.cpp
@@ -0,0 +1,38 @@
+// RUN: %clang -std=c++1z %s
+
+#include <experimental/meta>
+
+using namespace std::experimental;
+
+extern "C" int printf(const char*, ...);
+
+
+struct S {
+  int a, b, c;
+};
+
+
+template<meta::info X, typename T>
+bool compare(const T& a, const T& b) {
+  if constexpr (!is_null(X)) {
+    if constexpr (is_data_member(X)) {
+      auto p = valueof(X);
+      if (a.*p != b.*p)
+        return false;
+    }
+    return compare<next(X)>(a, b);
+  }
+  return true;
+}
+
+template<typename T>
+bool equal(const T& a, const T& b) {
+  return compare<front(reflexpr(T))>(a, b);
+}
+
+int main() {
+  S s1 { 0, 0, 0 };
+  S s2 { 0, 0, 1 };
+  assert(equal(s1, s1));
+  assert(!equal(s1, s2));
+}
