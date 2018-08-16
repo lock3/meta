9e87759de74ea2647a0ced4a662b486e5877185b
samgoodrick <samgoodrick@lock3software.com>
2018-08-16 12:54:10 -0400

    some testing of queries

---
+++ test/CXX/meta/context.cpp
@@ -0,0 +1,42 @@
+// RUN: %clang -std=c++1z %s
+
+#include <experimental/meta>
+
+using namespace std::experimental;
+
+namespace N1 { 
+  namespace N2 {
+    struct S { };
+  }
+  int x;
+}
+
+constexpr! int nesting(meta::info d) {
+  int n = 0;
+  meta::info p = parent(d);
+  while (!is_translation_unit(p)) {
+    p = parent(p);
+    ++n;
+  }
+  return n;
+}
+
+int main(int argc, char* argv[]) {
+
+  constexpr meta::info n1 = reflexpr(N1);
+  constexpr meta::info n2 = reflexpr(N1::N2);
+  constexpr meta::info s = reflexpr(N1::N2::S);
+  constexpr meta::info x = reflexpr(N1::x);
+
+  static_assert(parent(n2) == n1);
+  static_assert(parent(s) == n2);
+  static_assert(parent(x) == n1);
+
+  constexpr meta::info t1 = parent(n1);
+  static_assert(is_translation_unit(t1));
+
+  constexpr meta::info null = parent(t1);
+  static_assert(is_null(null));
+
+  static_assert(nesting(s) == 2);
+}
