ffb26afa552769ad8d2701c21ac8e9368c021af5
Wyatt Childers <wchilders@lock3software.com>
2018-08-29 16:54:40 -0400

    Added new test for source code fragments

---
+++ test/CXX/meta/fragment_namespace.cpp
@@ -0,0 +1,79 @@
+// RUN: %clang_cc1 -I%S/usr/include -I%S/usr/local/include/c++/v1 -fsyntax-only -verify -std=c++1z -freflection %s
+// expected-no-diagnostics
+
+#include <experimental/meta>
+
+namespace foo {
+
+constexpr auto test_cls_frag = __fragment class {
+  int i = 0;
+
+  void test_function() {
+  }
+};
+
+constexpr auto test_cls_self_frag = __fragment class Frag {
+  int i = 0;
+
+  void test_function(Frag& frag) {
+  }
+};
+
+constexpr auto test_struct_frag = __fragment struct {
+  int i = 0;
+
+  void test_function() {
+  }
+};
+
+constexpr auto test_struct_self_frag = __fragment struct Frag {
+  int i = 0;
+
+  void test_function(Frag& frag) {
+  }
+};
+
+constexpr auto test_union_frag = __fragment union {
+  int i = 0;
+
+  void test_function() {
+  }
+};
+
+constexpr auto test_union_self_frag = __fragment union Frag {
+  int i = 0;
+
+  void test_function(Frag& frag) {
+  }
+};
+
+constexpr auto test_ns_frag = __fragment namespace {
+  void test_function() {
+  }
+};
+
+// constexpr auto test_ns_self_frag = __fragment namespace Frag {
+//   void test_function(Frag& frag) {
+//   }
+// };
+
+template<auto &x_val>
+class SomeClass {
+  static constexpr auto cls_var = x_val;
+};
+
+constexpr auto test_cls_frag_wrapper = SomeClass<test_cls_frag>();
+constexpr auto test_cls_self_frag_wrapper = SomeClass<test_cls_self_frag>();
+constexpr auto test_struct_frag_wrapper = SomeClass<test_struct_frag>();
+constexpr auto test_struct_self_frag_wrapper = SomeClass<test_struct_self_frag>();
+constexpr auto test_union_frag_wrapper = SomeClass<test_union_frag>();
+constexpr auto test_union_self_frag_wrapper = SomeClass<test_union_self_frag>();
+constexpr auto test_ns_frag_wrapper = SomeClass<test_ns_frag>();
+// constexpr auto test_ns_self_frag_wrapper = SomeClass<test_ns_self_frag>();
+
+};
+
+int main() {
+  return 0;
+};
+
