3d63eaa1f00f4f5eab2caec5944b8b8be987ec1f
Matthias Gehre <gehre.matthias@gmail.com>
2018-09-01 23:46:29 +0200

    Hardcode support for _Ptr_base (base class of shared_ptr in MSVC)

--- lib/Analysis/LifetimeTypeCategory.cpp
+++ lib/Analysis/LifetimeTypeCategory.cpp
@@ -81,8 +81,11 @@ static bool satisfiesRangeConcept(const CXXRecordDecl *R) {
 // to look up the declarations (pointers) by names upfront and look up the
 // declarations instead of matching strings populated lazily.
 static Optional<TypeCategory> classifyStd(const Type *T) {
+  // MSVC: _Ptr_base is a base class of shared_ptr, and we only see
+  // _Ptr_base when calling get() on a shared_ptr.
   static std::set<StringRef> StdOwners{"stack",    "queue",   "priority_queue",
-                                       "optional", "variant", "any"};
+                                       "optional", "variant", "any",
+                                       "_Ptr_base"};
   static std::set<StringRef> StdPointers{"basic_regex", "reference_wrapper"};
   NamedDecl *Decl;
   if (const auto *TypeDef = T->getAs<TypedefType>()) {
