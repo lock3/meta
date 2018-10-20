4c4a53aaf1787149bdee1db897ef684a61c964b3
Wyatt Childers <wchilders@lock3software.com>
2018-10-20 15:55:50 -0400

    Added more complicated logic to the test file for (. .)

--- test/CXX/meta/reflected_id.cpp
+++ test/CXX/meta/reflected_id.cpp
@@ -1,21 +1,38 @@
 // RUN: %clang_cc1 -std=c++1z -freflection %s
 
-namespace bar { }
-
-template<typename T>
-T test() {
-  T (. "foo_", reflexpr(bar) .) = T();
-  return foo_bar;
+namespace bar {
+  namespace fin { }
 }
 
-struct S { };
+template<typename T>
+T test_template() {
+  constexpr auto int_reflexpr = reflexpr(int);
+  T (. "foo_", reflexpr(bar), "_", reflexpr(bar::fin) .) = T();
 
-void test_template() {
-  int int_val = test<int>();
-  S s_val = test<S>();
+  int int_x = foo_bar_fin.(. "get_", (. "int_reflexpr" .) .)();
+  int int_y = (. "foo_bar_fin" .).get_int();
+  int int_z = (. "foo_bar_fin" .).(. "get_int" .)();
+
+  return foo_bar_fin;
+}
+
+struct S {
+  int get_int() { return 0; }
+};
+
+S test_non_template() {
+  constexpr auto int_reflexpr = reflexpr(int);
+  S (. "foo_", reflexpr(bar), "_", reflexpr(bar::fin) .) = S();
+
+  int int_x = foo_bar_fin.(. "get_", (. "int_reflexpr" .) .)();
+  int int_y = (. "foo_bar_fin" .).get_int();
+  int int_z = (. "foo_bar_fin" .).(. "get_int" .)();
+
+  return foo_bar_fin;
 }
 
 int main() {
-  test_template();
+  S s_x = test_template<S>();
+  S s_y = test_non_template();
   return 0;
 }
