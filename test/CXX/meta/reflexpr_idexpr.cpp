b153440e4271855f7e17abde9e52511e1d97fca2
Andrew Sutton <asutton@lock3software.com>
2018-11-05 16:00:47 -0800

    Add missing test.

---
+++ test/CXX/meta/reflexpr_idexpr.cpp
@@ -0,0 +1,34 @@
+// RUN: %clang_cc1 -std=c++1z -freflection %s
+
+int global0 = 19;
+const int global1 = 75;
+constexpr int global2 = 76;
+
+enum E { A, B, C };
+
+struct S {
+  static constexpr int value = 4;
+  int num = 12;
+};
+
+constexpr S s1;
+
+int f() { return 0; }
+
+int main() {
+  // static_assert(idexpr(reflexpr(42)) == 42); // expected-error
+
+  static_assert(&idexpr(reflexpr(global0)) == &global0);
+  static_assert(idexpr(reflexpr(global1)) == 75);
+  
+  static_assert(idexpr(reflexpr(A)) == 0);
+  static_assert(idexpr(reflexpr(S::value)) == 4);
+  static_assert(&idexpr(reflexpr(S::value)) == &S::value);
+  
+  // static_assert(idexpr(reflexpr(s1.num)) == 12); // expected-error
+  
+  static_assert(idexpr(reflexpr(f)) == &f);
+  static_assert(idexpr(reflexpr(f)) == f);
+  
+  static_assert(s1.idexpr(reflexpr(S::num)) == 12);
+}
