0fc4ca9aca8ea2d8a0a70700ab6b0bcdce0f5044
Wyatt Childers <wchilders@lock3software.com>
2018-11-28 16:58:03 -0500

    Remove old tests and resolve new tests.

--- test/CXX/meta/meta-equal.cpp
+++ test/CXX/meta/meta-equal.cpp
@@ -1,15 +1,17 @@
-
+// RUN: %clang_cc1 -std=c++2a -stdlib=libc++ -freflection -fsyntax-only -verify %s
 struct S { };
 
 namespace N { }
 
-static_assert(reflexpr(N) == reflexpr(N));
-static_assert(reflexpr(N) != reflexpr(S));
-static_assert(reflexpr(int) == reflexpr(int));
-static_assert(reflexpr(int) != reflexpr(const int));
+int main() {
+  static_assert(reflexpr(N) == reflexpr(N));
+  static_assert(reflexpr(N) != reflexpr(S));
+  static_assert(reflexpr(int) == reflexpr(int));
+  static_assert(reflexpr(int) != reflexpr(const int));
 
-static_assert(reflexpr(int) < reflexpr(int)); // expected-error {{invalid operands}}
-static_assert(reflexpr(int) > reflexpr(int)); // expected-error {{invalid operands}}
-static_assert(reflexpr(int) <= reflexpr(int)); // expected-error {{invalid operands}}
-static_assert(reflexpr(int) >= reflexpr(int)); // expected-error {{invalid operands}}
-static_assert(reflexpr(int) <=> reflexpr(int)); // expected-error {{invalid operands}}
+  static_assert(reflexpr(int) < reflexpr(int)); // expected-error {{invalid operands to binary expression ('meta::info' and 'meta::info')}}
+  static_assert(reflexpr(int) > reflexpr(int)); // expected-error {{invalid operands to binary expression ('meta::info' and 'meta::info')}}
+  static_assert(reflexpr(int) <= reflexpr(int)); // expected-error {{invalid operands to binary expression ('meta::info' and 'meta::info')}}
+  static_assert(reflexpr(int) >= reflexpr(int)); // expected-error {{invalid operands to binary expression ('meta::info' and 'meta::info')}}
+  static_assert(reflexpr(int) <=> reflexpr(int)); // expected-error {{invalid operands to binary expression ('meta::info' and 'meta::info')}}
+}
