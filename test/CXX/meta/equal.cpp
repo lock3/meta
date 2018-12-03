3e89d6c88c2d9b12380c618cffb716034fd63865
Wyatt Childers <wchilders@lock3software.com>
2018-12-03 11:22:58 -0500

    Update the equality tests

--- test/CXX/meta/equal.cpp
+++ test/CXX/meta/equal.cpp
@@ -1,6 +1,36 @@
 // RUN: %clang_cc1 -std=c++1z -freflection %s
 
-extern "C" int printf(const char*, ...);
+#include "reflection_query.h"
+
+#define assert(E) if (!(E)) __builtin_abort();
+
+namespace meta {
+
+using info = decltype(reflexpr(void));
+
+consteval bool is_invalid(info m) {
+  return __reflect(query_is_invalid, m);
+}
+
+consteval bool is_data_member(info m) {
+  if (__reflect(query_is_nonstatic_data_member, m))
+    return true;
+
+  if (__reflect(query_is_static_data_member, m))
+    return true;
+
+  return false;
+}
+
+consteval info front(info x) {
+  return __reflect(query_get_begin, x);
+}
+
+consteval info next(info x) {
+  return __reflect(query_get_next, x);
+}
+
+} // end namespace meta
 
 struct S {
   int a, b, c;
@@ -8,20 +38,20 @@ struct S {
 
 template<meta::info X, typename T>
 bool compare(const T& a, const T& b) {
-  if constexpr (!is_null(X)) {
-    if constexpr (is_data_member(X)) {
+  if constexpr (!meta::is_invalid(X)) {
+    if constexpr (meta::is_data_member(X)) {
       auto p = valueof(X);
       if (a.*p != b.*p)
         return false;
     }
-    return compare<next(X)>(a, b);
+    return compare<meta::next(X)>(a, b);
   }
   return true;
 }
 
 template<typename T>
 bool equal(const T& a, const T& b) {
-  return compare<front(reflexpr(T))>(a, b);
+  return compare<meta::front(reflexpr(T))>(a, b);
 }
 
 int main() {
