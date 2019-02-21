0d0af4ed9901acf6b7a09bde15abff815d50f6d5
Wyatt Childers <wchilders@lock3software.com>
2019-02-21 10:40:59 -1000

    Test the __concatenate intrinsic with type aliases

--- test/CXX/meta/concatenate-intrinsic.cpp
+++ test/CXX/meta/concatenate-intrinsic.cpp
@@ -1,6 +1,8 @@
 // RUN: %clang_cc1 -freflection -std=c++1z %s
 
-constexpr bool string_eq(const char *s1, const char *s2) {
+using string_type = const char *;
+
+constexpr bool string_eq(string_type s1, string_type s2) {
   while (*s1 != '\0' && *s1 == *s2) {
     s1++;
     s2++;
@@ -9,7 +11,7 @@ constexpr bool string_eq(const char *s1, const char *s2) {
   return *s1 == *s2;
 }
 
-constexpr const char * join_strings(const char *s1, const char *s2) {
+constexpr string_type join_strings(string_type s1, string_type s2) {
   return __concatenate(true ? s1 : "false", " ", s2);
 }
 
