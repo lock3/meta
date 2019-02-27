d8e648b594432897fed6488240951d92e34e9a89
samgoodrick <samgoodrick@lock3software.com>
2019-02-27 16:30:44 -0500

    tests added for struct expansion

--- test/CXX/meta/expansion.cpp
+++ test/CXX/meta/expansion.cpp
@@ -87,6 +87,22 @@ struct range
   // constexpr static int size() { return N; }
 };
 
+// Struct expansion stuff
+template<typename T, typename S, typename U>
+struct record {
+  T t1 = T();
+  S s1 = S();
+  U u1 = U();
+
+  struct nested {
+    T skipped;
+  };
+
+  T t2 = T();
+  U f() { return U(); }
+  S s2 = S();
+};
+
 
 
 // Tests
@@ -99,13 +115,13 @@ void test_array() {
 
 void test_constexpr_array() {
   static constexpr int arr[] = { 1, 2, 3, 4 };
-  for constexpr (int a : arr)
+  for... (constexpr int a : arr)
     ;
 }
 
 constexpr int global_arr[] = { 0, 1 };
 void test_constexpr_array_2() {
-  for constexpr (int a : global_arr)
+  for... (constexpr int a : global_arr)
     ;
 }
 
@@ -115,20 +131,40 @@ void test_tuple() {
     ;
 }
 
+template <typename T, typename U, typename S>
+void test_record() {
+  record<T, U, S> r;
+  for... (auto x : r)
+    ;
+}
+
 void test_constexpr_tuple() {
   static constexpr tuple tup;
-  for constexpr (auto x : tup)
+  for... (constexpr auto x : tup)
     ;
 }
 
 void test_constexpr_range() {
   static constexpr range<7> ints;
-  for constexpr (int n : ints)
+  for... (constexpr int n : ints)
+    ;
+}
+
+template <typename T, typename U, typename S>
+void test_constexpr_record() {
+  static constexpr record<T, U, S> r;
+  for... (constexpr auto field : r)
     ;
 }
 
 int main() {
   test_array();
+  test_tuple();
+  test_record<int, double, char>();
   test_constexpr_array();
+  test_constexpr_array_2();
+  test_constexpr_tuple();
+  test_constexpr_range();
+  test_constexpr_record<int, double, char>();
 }
 
