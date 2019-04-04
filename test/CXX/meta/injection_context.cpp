c92f9cb39a7687fb8235fce0d7104dec1deaba71
Wyatt Childers <wchilders@lock3software.com>
2019-04-04 11:48:59 -0400

    Updated tests to include testing for enum fragment injection

--- test/CXX/meta/injection_context.cpp
+++ test/CXX/meta/injection_context.cpp
@@ -1,16 +1,24 @@
 // RUN: %clang_cc1 -verify -freflection -std=c++1z %s
 
-constexpr auto namespace_frag = __fragment namespace {
-};
+constexpr auto namespace_frag = __fragment namespace { };
+constexpr auto class_frag = __fragment class { };
+constexpr auto enum_frag = __fragment enum { };
+
+namespace ns {
+  consteval -> namespace_frag;
+  consteval -> class_frag; // expected-error {{injecting class members into a namespace}}
+  consteval -> enum_frag; // expected-error {{injecting enum members into a namespace}}
+}
 
 class clazz {
   consteval -> namespace_frag; // expected-error {{injecting namespace members into a class}}
+  consteval -> class_frag;
+  consteval -> enum_frag; // expected-error {{injecting enum members into a class}}
 };
 
-constexpr auto class_frag = __fragment class {
+enum enam {
+  consteval -> namespace_frag, // expected-error {{injecting namespace members into an enum}}
+  consteval -> class_frag, // expected-error {{injecting class members into an enum}}
+  consteval -> enum_frag
 };
-
-namespace ns {
-  consteval -> class_frag; // expected-error {{injecting class members into a namespace}}
-}
 
