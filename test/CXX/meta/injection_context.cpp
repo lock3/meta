edc82eec27a589220760f569abd919d8d467b806
Wyatt Childers <wchilders@lock3software.com>
2019-04-04 12:17:11 -0400

    Updated tests to include testing for block fragment injection

--- test/CXX/meta/injection_context.cpp
+++ test/CXX/meta/injection_context.cpp
@@ -3,22 +3,34 @@
 constexpr auto namespace_frag = __fragment namespace { };
 constexpr auto class_frag = __fragment class { };
 constexpr auto enum_frag = __fragment enum { };
+// FIXME: The lambda shouldn't be necessary here
+constexpr auto block_frag = [] () -> auto { return __fragment { }; }();
 
 namespace ns {
   consteval -> namespace_frag;
   consteval -> class_frag; // expected-error {{injecting class members into a namespace}}
   consteval -> enum_frag; // expected-error {{injecting enum members into a namespace}}
+  consteval -> block_frag; // expected-error {{injecting statements into a namespace}}
 }
 
 class clazz {
   consteval -> namespace_frag; // expected-error {{injecting namespace members into a class}}
   consteval -> class_frag;
   consteval -> enum_frag; // expected-error {{injecting enum members into a class}}
+  consteval -> block_frag; // expected-error {{injecting statements into a class}}
 };
 
 enum enam {
   consteval -> namespace_frag, // expected-error {{injecting namespace members into an enum}}
   consteval -> class_frag, // expected-error {{injecting class members into an enum}}
-  consteval -> enum_frag
+  consteval -> enum_frag,
+  consteval -> block_frag // expected-error {{injecting statements into an enum}}
 };
+
+void func() {
+  consteval -> namespace_frag; // expected-error {{injecting namespace members into a function}}
+  consteval -> class_frag; // expected-error {{injecting class members into a function}}
+  consteval -> enum_frag; // expected-error {{injecting enum members into a function}}
+  consteval -> block_frag;
+}
 
