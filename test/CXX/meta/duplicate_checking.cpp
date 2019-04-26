aa3325816080c709aa3f103864ed010bdcefd651
Wyatt Childers <wchilders@lock3software.com>
2019-04-26 13:08:41 -0400

    Added duplicate checking for function declarations

--- test/CXX/meta/duplicate_checking.cpp
+++ test/CXX/meta/duplicate_checking.cpp
@@ -1,9 +1,20 @@
 // RUN: %clang_cc1 -verify -fsyntax-only -freflection -std=c++1z %s
 
-struct Foo {
-  int method_a() { } // expected-note {{previous definition is here}}
+class C {
+  void foo() { } // expected-note {{previous definition is here}}
+  void bar() { } // expected-note {{previous definition is here}}
 
-  consteval -> __fragment struct {
-    int method_a() { } // expected-error {{class member cannot be redeclared}}
-  };
+  consteval -> __fragment class {
+    void foo() { } // expected-error {{class member cannot be redeclared}}
+    int bar() { return 0; } // expected-error {{functions that differ only in their return type cannot be overloaded}}
+  }
+};
+
+void foo() { } // expected-note {{previous definition is here}}
+void bar() { } // expected-note {{previous definition is here}}
+
+consteval -> __fragment namespace {
+  void foo();
+  void foo() { } // expected-error {{redefinition of 'foo'}}
+  int bar() { return 0; } // expected-error {{functions that differ only in their return type cannot be overloaded}}
 };
