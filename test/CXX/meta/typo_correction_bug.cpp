76f80b95dea5757234cf129039cdf9103a64cec7
Wyatt Childers <wchilders@lock3software.com>
2019-05-23 11:34:16 -0400

    Add missing test for previous bugfix commit

---
+++ test/CXX/meta/typo_correction_bug.cpp
@@ -0,0 +1,20 @@
+// RUN: %clang_cc1 -freflection -std=c++2a -fsyntax-only -verify %s
+
+namespace meta {
+  using info = decltype(reflexpr(void));
+}
+
+template <typename... Args>
+void dummy(Args...) {}
+
+consteval void impl(meta::info source) {
+  int a = 1;
+  -> __fragment struct S {
+    void foo() {
+      dummy(unqualid("foo_", a)); // expected-error {{use of undeclared identifier 'foo_1'}}
+    }
+  };
+}
+
+class(impl) foo {
+};
