6601fa9b27a4eaef6577a55fdb17e333e46a9ef3
Gabor Horvath <xazax.hun@gmail.com>
2019-08-09 18:12:12 +0000

    Disable statement local analysis for godbol test.

--- clang/test/Sema/warn-lifetime-godbolt.cpp
+++ clang/test/Sema/warn-lifetime-godbolt.cpp
@@ -1,5 +1,5 @@
-// RUN: %clang -std=c++1z -fsyntax-only -Xclang -verify -Wlifetime -isystem %S/../../range-v3/include -Wno-return-stack-address -stdlib=libstdc++ %s
-// RUN: %clang -std=c++1z -fsyntax-only -Xclang -verify -Wlifetime -isystem %S/../../range-v3/include -Wno-return-stack-address -stdlib=libc++ -I/usr/lib/llvm-9/include/c++/v1 %s
+// RUN: %clang -std=c++1z -fsyntax-only -Xclang -verify -Wlifetime -isystem %S/../../range-v3/include -Wno-return-stack-address -Wno-dangling -stdlib=libstdc++ %s
+// RUN: %clang -std=c++1z -fsyntax-only -Xclang -verify -Wlifetime -isystem %S/../../range-v3/include -Wno-return-stack-address -Wno-dangling -stdlib=libc++ -I/usr/lib/llvm-9/include/c++/v1 %s
 // Note: paths are for Ubuntu's libc++-9-dev package from http://apt.llvm.org/
 
 // This is intedend to contain all examples that are linked from the paper.
