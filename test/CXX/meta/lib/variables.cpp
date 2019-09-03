b70b983fedea2eb7b4ab0b310e9b1b13750a851b
Wyatt Childers <wchilders@lock3software.com>
2019-09-03 17:21:52 -0400

    Write all predicates as function objects

--- test/CXX/meta/lib/variables.cpp
+++ test/CXX/meta/lib/variables.cpp
@@ -7,14 +7,14 @@ int x;
 static_assert(__reflect(query_is_variable, reflexpr(x)));
 static_assert(__reflect(query_has_static_storage, reflexpr(x)));
 static_assert(!__reflect(query_has_thread_local_storage, reflexpr(x)));
-static_assert(!__reflect(query_has_automatic_local_storage, reflexpr(x)));
+static_assert(!__reflect(query_has_automatic_storage, reflexpr(x)));
 
 thread_local int k;
 
 static_assert(__reflect(query_is_variable, reflexpr(k)));
 static_assert(!__reflect(query_has_static_storage, reflexpr(k)));
 static_assert(__reflect(query_has_thread_local_storage, reflexpr(k)));
-static_assert(!__reflect(query_has_automatic_local_storage, reflexpr(k)));
+static_assert(!__reflect(query_has_automatic_storage, reflexpr(k)));
 
 void local() {
   int y;
@@ -22,7 +22,7 @@ void local() {
   static_assert(__reflect(query_is_variable, reflexpr(y)));
   static_assert(!__reflect(query_has_static_storage, reflexpr(y)));
   static_assert(!__reflect(query_has_thread_local_storage, reflexpr(y)));
-  static_assert(__reflect(query_has_automatic_local_storage, reflexpr(y)));
+  static_assert(__reflect(query_has_automatic_storage, reflexpr(y)));
 }
 
 static_assert(!__reflect(query_is_variable, reflexpr(local)));
