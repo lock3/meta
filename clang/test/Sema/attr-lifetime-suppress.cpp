dd532b5f9eaf31900798772f1ece57e8a4aa24e2
Lars Klein <larsklein53@gmail.com>
2019-10-22 22:05:23 +0200

    Adjust test case to make it simpler

--- clang/test/Sema/attr-lifetime-suppress.cpp
+++ clang/test/Sema/attr-lifetime-suppress.cpp
@@ -1,42 +1,23 @@
 // RUN: %clang_cc1 -fcxx-exceptions -fsyntax-only -verify -Wlifetime -Wno-dangling %s
 
-namespace {
-template <class T>
-class [[gsl::Owner(T *)]] unique_ptr {
-public:
-  unique_ptr(T * t) : t(t) {}
-
-  T *release() {
-    T *tmp = t;
-    t = nullptr;
-    return tmp;
-  }
-
-  T *t = nullptr;
-};
-
-class PrivateKey {
-  // A random class that models a private key.
-};
-} // namespace
-
 namespace nosuppressOnFunction {
-PrivateKey *getPrivateKey() {
-  return unique_ptr<PrivateKey>(new PrivateKey()).release(); // expected-warning {{returning a dangling pointer}}
-  // expected-note@-1 {{temporary was destroyed at the end of the full expression}}
+int *f() {
+  int *p; // expected-note {{it was never initialized here}}
+  return p; // expected-warning {{returning a dangling pointer}}
 }
 } // namespace nosuppressOnFunction
 
 namespace suppressOnFunction {
 
-[[gsl::suppress("lifetime")]] PrivateKey *getPrivateKey() {
-  return unique_ptr<PrivateKey>(new PrivateKey()).release(); // OK
+[[gsl::suppress("lifetime")]] int *f() {
+  int *p;
+  return p; // OK
 }
 
 namespace suppressNotLifetime {
-[[gsl::suppress("notLifetime")]] PrivateKey *getPrivateKey() {
-  return unique_ptr<PrivateKey>(new PrivateKey()).release(); // expected-warning {{returning a dangling pointer}}
-  // expected-note@-1 {{temporary was destroyed at the end of the full expression}}
+[[gsl::suppress("notLifetime")]] int *f() {
+  int *p; // expected-note {{it was never initialized here}}
+  return p; // expected-warning {{returning a dangling pointer}}
 }
 } // namespace suppressNotLifetime
 } // namespace suppressOnFunction
