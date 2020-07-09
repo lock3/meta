526a85f514f2b8ae0c9dd3cc391c79a0b1c05fd3
Wyatt Childers <wchilders@lock3software.com>
2020-07-09 15:18:48 -0400

    WIP - Add support for dependent template identifiers

--- clang/test/CXX/meta/clang/splice.identifier/template-identifiers.cpp
+++ clang/test/CXX/meta/clang/splice.identifier/template-identifiers.cpp
@@ -16,3 +16,40 @@ void test() {
 
 } // end namespace class_name
 
+namespace dependent_class_name {
+
+template<int N>
+struct S1 {
+  static int X2 () {
+    return N;
+  }
+};
+
+template<int Y>
+void templ_test() {
+  int q = typename unqualid("S", Y)<2>::X2();
+}
+
+void test() {
+  templ_test<1>();
+}
+
+} // end namespace dependent_class_name
+
+namespace dependent_namespace_name {
+
+namespace S1 {
+  class C {
+  };
+}
+template<int Y>
+void templ_test() {
+  auto c = unqualid("S", Y)::C();
+}
+
+void test() {
+  templ_test<1>();
+}
+
+} // end namespace dependent_class_name
+
