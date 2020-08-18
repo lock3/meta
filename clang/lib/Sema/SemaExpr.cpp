4b0fe9127c97e4c43f8583137887eb7f8f6495e3
Andrew Sutton <asutton@lock3software.com>
2020-08-18 10:18:36 -0400

    Remove redundant assignment.
    And fix a misleading comment.

--- clang/lib/Sema/SemaExpr.cpp
+++ clang/lib/Sema/SemaExpr.cpp
@@ -3308,9 +3308,8 @@ ExprValueKind Sema::getValueKindForDeclReference(QualType &type, ValueDecl *VD,
       break;
 
     case Type::MoveParameter:
-      // The type is adjusted to an xvalue of type T.
+      // The type is adjusted to T. It's already an lvalue.
       type = cast<ParameterType>(type)->getParameterType();
-      valueKind = VK_LValue;
       break;
 
     default:
