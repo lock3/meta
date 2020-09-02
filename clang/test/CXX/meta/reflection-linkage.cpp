475624f3814c91ffe5f0d4e08171fdfb1065c758
Wyatt Childers <wchilders@lock3software.com>
2020-09-02 19:14:00 -0400

    Fixed crash related to debug mode

--- clang/test/CXX/meta/reflection-linkage.cpp
+++ clang/test/CXX/meta/reflection-linkage.cpp
@@ -1,4 +1,5 @@
 // RUN: %clang_cc1 -std=c++2a -freflection -emit-llvm -triple %itanium_abi_triple -o - %s | FileCheck %s
+// RUN: %clang_cc1 -std=c++2a -freflection -debug-info-kind=limited -emit-llvm -triple %itanium_abi_triple -o - %s | FileCheck %s
 
 #include "reflection_query.h"
 
